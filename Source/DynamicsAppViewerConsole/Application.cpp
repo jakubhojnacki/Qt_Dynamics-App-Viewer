2b9cf430e3aec8c7b5df15c09583ab42fd91a97d
Jakub Hojnacki <jakubhojnacki@gmail.com>
2022-12-10 21:24:26 +0000

    Small mods to command line 2

--- Source/DynamicsAppViewerConsole/Application.cpp
+++ Source/DynamicsAppViewerConsole/Application.cpp
@@ -1,36 +1,42 @@
 #include <QCommandLineParser>
 #include <QTextStream>
+
 #include "Application.hpp"
 #include "Formatters/ApplicationInformationFormatter.hpp"
 #include "Formatters/ArgumentsFormatter.hpp"
 #include "General/ArgumentParser.hpp"
 
-namespace Fortah { namespace DynamicsAppViewer { namespace Console {
-    Application::Application(QObject* pParent) : QObject(pParent) {
-    }
+namespace Fortah {
+    namespace DynamicsAppViewer {
+        namespace Console {
+            Application::Application(QObject* pParent) :
+                QObject { pParent } {
+            }
 
-    QCoreApplication* Application::getQtApplication() {
-        return static_cast<QCoreApplication*>(this->parent());
-    }
+            QCoreApplication* Application::getQtApplication() {
+                return static_cast<QCoreApplication*>(this->parent());
+            }
 
-    void Application::run() {
-        this->initialise();
-        this->readArguments();
-        this->finalise();
-    }
+            void Application::run() {
+                this->initialise();
+                this->readArguments();
+                this->finalise();
+            }
 
-    void Application::initialise() {
-        this->out << Formatters::ApplicationInformationFormatter::toString(this->applicationInformation) << "\r\n";
-    }
+            void Application::initialise() {
+                this->out << Formatters::ApplicationInformationFormatter::toString(this->applicationInformation) << "\r\n";
+            }
 
-    void Application::readArguments() {
-        General::ArgumentParser parser { this->library.getApplicationInformation() };
-        this->arguments = parser.parse(*this->getQtApplication());
-        this->out << Formatters::ArgumentsFormatter::toString(this->arguments) << "\r\n";
-    }
+            void Application::readArguments() {
+                General::ArgumentParser parser { this->library.getApplicationInformation() };
+                this->arguments = parser.parse(*this->getQtApplication());
+                this->out << Formatters::ArgumentsFormatter::toString(this->arguments) << "\r\n";
+            }
 
-    void Application::finalise() {
-        this->out << "Completed.\r\n";
-        emit finished();
+            void Application::finalise() {
+                this->out << "Completed.\r\n";
+                emit finished();
+            }
+        }
     }
-} } }
+}
--- Source/DynamicsAppViewerConsole/Formatters/ApplicationInformationFormatter.cpp
+++ Source/DynamicsAppViewerConsole/Formatters/ApplicationInformationFormatter.cpp
@@ -1,8 +1,17 @@
 #include "ApplicationInformationFormatter.hpp"
 
-namespace Fortah { namespace DynamicsAppViewer { namespace Console { namespace Formatters {
-    QString ApplicationInformationFormatter::toString(const Core::General::ApplicationInformation& pApplicationInformation) {
-        return QString("%1 %2 - %3 - %4 - %5").arg(pApplicationInformation.getName(), pApplicationInformation.getVersion().toString(),
-            pApplicationInformation.getDescription(), pApplicationInformation.getAuthor(), pApplicationInformation.getDate());
+namespace Fortah {
+    namespace DynamicsAppViewer {
+        namespace Console {
+            namespace Formatters {
+                ApplicationInformationFormatter::ApplicationInformationFormatter() {
+                }
+
+                QString ApplicationInformationFormatter::toString(const Core::General::ApplicationInformation& pApplicationInformation) {
+                    return QString { "%1 %2 - %3 - %4 - %5" }.arg(pApplicationInformation.getName(), pApplicationInformation.getVersion().toString(),
+                        pApplicationInformation.getDescription(), pApplicationInformation.getAuthor(), pApplicationInformation.getDate());
+                }
+            }
+        }
     }
-} } } }
+}
